b7535c16d34698a703bedfbf91ee450bb49c2072
Rubel Hosen <cserubelhosen@gmail.com>
2024-12-09 13:17:07 +0600

    carousel add & card

--- views/home.php
+++ views/home.php
@@ -7,17 +7,108 @@
     <link rel="stylesheet" href="assets/css/bootstrap.css">   
 </head>
 <body>
-    <nav class="navbar">
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
-            <li><a href="">Portfolio</a></li>
-            <li><a href="">Gallary</a></li>
-            <li><a href="">Gallary</a></li>
-        </ul>
+    <nav class="navbar navbar-expand bg-dark navbar-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">My Portfolio</a>
+            <ul class="navbar-nav mx-auto">
+                <li><a href=""class="nav-link">Home</a></li>
+                <li><a href="" class="nav-link">About</a></li>
+                <li><a href="" class="nav-link">Contact</a></li>
+                <li><a href="" class="nav-link">Portfolio</a></li>
+                <li><a href=""  class="nav-link">Gallary</a></li>
+                <li><a href="" class="nav-link">Gallary</a></li>
+            </ul>
+            <ul class="navbar-nav">
+                <li><a href="" class="nav-link">Login</a> </li>
+                <li><a href="" class="nav-link">Sign Up</a> </li>
+            </ul>
+
+        </div>
+
            
     </nav>
+
+
+    <div id="slider" class="carousel slide" data-bs-ride="carousel" data-bs-interval="1800">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/img/web.jfif" alt="image" class="w-100" height="550">
+                <div class="carousel-caption">
+                    <h1>Heading</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut consequuntur cumque deleniti, earum, esse facilis illum magnam minima optio quis recusandae reiciendis repellat sequi voluptatem. Aut ex hic recusandae?</p>
+                    <a href="" class="btn btn-success">read more</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="assets/img/ww.webp" alt="image" class="w-100" height="550">
+                <div class="carousel-caption">
+                    <h1>Heading</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut consequuntur cumque deleniti, earum, esse facilis illum magnam minima optio quis recusandae reiciendis repellat sequi voluptatem. Aut ex hic recusandae?</p>
+                    <a href="" class="btn btn-success">read more</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="assets/img/7.png" alt="image" class="w-100" height="550">
+                <div class="carousel-caption">
+                    <h1>Heading</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut consequuntur cumque deleniti, earum, esse facilis illum magnam minima optio quis recusandae reiciendis repellat sequi voluptatem. Aut ex hic recusandae?</p>
+                    <a href="" class="btn btn-success">read more</a>
+                </div>
+            </div>
+
+        </div>
+
+        <a href="#slider" class="carousel-control-prev" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a href="#slider" class="carousel-control-next" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+
+    </div>
+
+
+
+    <section class="py-5 bg-info">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="assets/img/s6.png" alt="">
+                        <div class="card-body">
+                            <h4>This is Heading Content</h4>
+                            <p>Lorem ipsum is the best </p>
+                            <a href="" class="btn btn-dark">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="assets/img/s6.png" alt="">
+                        <div class="card-body">
+                            <h4>This is Heading Content</h4>
+                            <p>Lorem ipsum is the best </p>
+                            <a href="" class="btn btn-dark">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="assets/img/s6.png" alt="">
+                        <div class="card-body">
+                            <h4>This is Heading Content</h4>
+                            <p>Lorem ipsum is the best </p>
+                            <a href="" class="btn btn-dark">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </section>>
+
     <script src="assets/js/bootstrap.bundle.js"></script>
 </body>
 </html>
